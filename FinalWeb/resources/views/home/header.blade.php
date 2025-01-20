14b6962254d5d356398d3ae83322a37b67d6253c
SaishBhattarai <bsaish22@tbc.edu.np>
2025-01-21 00:58:24 +0545

    crated cash order

--- FinalWeb/resources/views/home/header.blade.php
+++ FinalWeb/resources/views/home/header.blade.php
@@ -1,46 +1,49 @@
-<!-- header section strats -->
+<!-- header section starts -->
 <header class="header_section">
-            <div class="container">
-               <nav class="navbar navbar-expand-lg custom_nav-container ">
-                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class=""> </span>
-                  </button>
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                     <ul class="navbar-nav">
-                        <li class="nav-item active">
-                           <a class="nav-link" href="{{url('/')}}">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                       
-                        <li class="nav-item">
-                           <a class="nav-link" href="product.blade.php">Products</a>
-                        </li>
-                        
-                        <li class="nav-item">
-                           <a class="nav-link" href="{{url('show_cart')}}">Cart</a>
-                        </li>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container">
+            <!-- Navbar toggler for mobile devices -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Navbar links -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto"> <!-- Use ml-auto to align items to the right -->
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="product.blade.php">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('show_cart') }}">Cart</a>
+                    </li>
+                    <!-- Search form -->
+                    <li class="nav-item">
                         <form class="form-inline">
-                           <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
-                           <i class="fa fa-search" aria-hidden="true"></i>
-                           </button>
+                            <button class="btn my-2 my-sm-0 nav_search-btn" type="submit">
+                                <i class="fa fa-search" aria-hidden="true"></i>
+                            </button>
                         </form>
-                        @if (Route::has('login'))
+                    </li>
+                    <!-- Authentication links (login/register) -->
+                    @if (Route::has('login'))
                         @auth
-                        <li class="nav-item">
-                         <x-app-layout>
-                         </x-app-layout>
-                        </li>
+                            <li class="nav-item">
+                                <x-app-layout></x-app-layout>
+                            </li>
                         @else
-                        <li class="nav-item">
-                           <a class="btn btn-primary" id="logincss" href="{{ route('login') }}">Login</a>
-                        </li>
-                        <li class="nav-item">
-                           <a class="btn btn-success" href="{{ route('register') }}">Register</a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="btn btn-primary" id="logincss" href="{{ route('login') }}">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="btn btn-success" href="{{ route('register') }}">Register</a>
+                            </li>
                         @endauth
-                        @endif
-                     </ul>
-                  </div>
-               </nav>
+                    @endif
+                </ul>
             </div>
-         </header>
-         <!-- end header section -->
+        </nav>
+    </div>
+</header>
+<!-- end header section -->
