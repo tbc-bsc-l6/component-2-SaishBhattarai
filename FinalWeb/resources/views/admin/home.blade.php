6af5ef16205c316c3058eb87ec627e397d0e64f7
SaishBhattarai <bsaish22@tbc.edu.np>
2025-01-20 11:16:13 +0545

    crated admin dashboard

--- FinalWeb/resources/views/admin/home.blade.php
+++ FinalWeb/resources/views/admin/home.blade.php
@@ -1,15 +1,65 @@
-<x-app-layout>
- 
-</x-app-layout>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
-</head>
-<body>
-    
-</body>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Corona Admin</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="Admin/assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="Admin/assets/vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="Admin/assets/vendors/jvectormap/jquery-jvectormap.css">
+    <link rel="stylesheet" href="Admin/ssets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="Admin/assets/vendors/owl-carousel-2/owl.carousel.min.css">
+    <link rel="stylesheet" href="Admin/assets/vendors/owl-carousel-2/owl.theme.default.min.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="Admin/assets/css/style.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="Admin/assets/images/favicon.png" />
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- sidebar -->
+       @include('admin.sidebar')
+      <!-- sidebar -->
+      <!-- navbar -->
+      @include('admin.navbar')
+      <!-- navbar -->  
+      <!-- body -->
+      @include('admin.body')
+      <!-- body -->
+        
+          
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="Admin/assets/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="Admin/assets/vendors/chart.js/Chart.min.js"></script>
+    <script src="Admin/assets/vendors/progressbar.js/progressbar.min.js"></script>
+    <script src="Admin/assets/vendors/jvectormap/jquery-jvectormap.min.js"></script>
+    <script src="Admin/assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="Admin/assets/vendors/owl-carousel-2/owl.carousel.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="Admin/assets/js/off-canvas.js"></script>
+    <script src="Admin/assets/js/hoverable-collapse.js"></script>
+    <script src="Admin/assets/js/misc.js"></script>
+    <script src="Admin/assets/js/settings.js"></script>
+    <script src="Admin/assets/js/todolist.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="Admin/assets/js/dashboard.js"></script>
+    <!-- End custom js for this page -->
+  </body>
 </html>
